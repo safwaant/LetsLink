90da823042d415f93d51b7ae2a8053bf7c550b63
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-02-20 18:42:34 -0800

    Add React.js and seperate folders in frontend / backend for organizing

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const app = require('express');
+
+
+
+
+setTimeout(() => {
+    console.log('Tell her that she beautiful every day I remiiind her');
+}, 200);
+
+console.log('Shorty like a thousand dollar plate fine chinaaaa');
