2badeb30a2f5fdd078cdcf5122754f8d41972da4
Veronika Bykova <90811939+bykovav8@users.noreply.github.com>
2022-03-09 20:02:34 -0800

    added react and frontend skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {TextField, Button} from '@material-ui/core';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignIn from './LetsLinkUI/Sign-In/SignIn';
+import HomePage from './LetsLinkUI/Home-Page/HomePage';
+import Calendar from 'react-calendar';
 
 function App() {
   let [dataFetched, setDataFetched] = useState(false);
@@ -26,32 +29,41 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-        {dataFetched &&
-            <div>
-              <h2> All current members of LetsLink:</h2>
-              <ul>
-              {persons.map((person) => {
-                return(<li>{person.person_name}</li>)
-              })}
-              </ul>
-            </div>
-        }
-        </div>
-        <div style={{verticalAlign: "middle"}}>
-          <TextField value={addInput} 
-                    id="outlined-basic" 
-                    color="white" 
-                    label="Name" 
-                    variant="filled" 
-                    onChange={(e) => setAddInput(e.target.value)}/>
-          <Button variant="contained" onClick={() => alert(addInput)} size="large">Submit</Button>
-        </div>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <div>
+    //     {dataFetched &&
+    //         <div>
+    //           <h2> All current members of LetsLink:</h2>
+    //           <ul>
+    //           {persons.map((person) => {
+    //             return(<li>{person.person_name}</li>)
+    //           })}
+    //           </ul>
+    //         </div>
+    //     }
+    //     </div>
+    //     <div style={{verticalAlign: "middle"}}>
+    //       <TextField value={addInput} 
+    //                 id="outlined-basic" 
+    //                 color="white" 
+    //                 label="Name" 
+    //                 variant="filled" 
+    //                 onChange={(e) => setAddInput(e.target.value)}/>
+    //       <Button variant="contained" onClick={() => alert(addInput)} size="large">Submit</Button>
+    //     </div>
+    //   </header>
+    // </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<SignIn/>}/>
+          <Route exact path="/home" element={<HomePage/>}/>
+          <Route exact path="/calendar" element={<Calendar/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
